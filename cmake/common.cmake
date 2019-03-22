c561958b90bf17c2726a94f90a712b35f211977e
Cristian Petruta <crispet@ntdev.microsoft.com>
2019-03-22 10:44:59 -0700

    Remove AVX

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -31,7 +31,7 @@ endfunction()
 
 function(_set_compile_options proj)
     if(WIN32)
-        target_compile_options(${proj} PRIVATE /arch:AVX /Zi)
+        target_compile_options(${proj} PRIVATE  /Zi)
         target_compile_definitions(${proj} PRIVATE __WINDOWS_WASAPI__=1)
         # TODO: These vars are for libniquist and should be set in the find libynquist script.
         target_compile_definitions(${proj} PRIVATE HAVE_STDINT_H=1 HAVE_SINF=1)
